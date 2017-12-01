fbb024fcb42a9664f12b8d97e7b96f61705b4a24
karlesleith <karlesleith@gmail.com>
2017-12-01 23:03:11 +0000

    Added MNIST Model

---
+++ mnist.py
@@ -0,0 +1,74 @@
+# Karle Sleith G00324919	
+# Refrences
+# https://github.com/salmanahmad4u/keras-iris/blob/master/iris_nn.py
+#https://github.com/yashk2810/MNIST-Keras/blob/master/Notebook/MNIST_keras_CNN-99.55%25.ipynb
+import numpy as np
+from keras.datasets import mnist
+import keras as kr
+from keras.layers.advanced_activations import LeakyReLU
+from keras.preprocessing.image import ImageDataGenerator
+
+# Loading the MNIST Dataset
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+
+print("X_train original shape", x_train.shape)
+print("y_train original shape", y_train.shape)
+print("X_test original shape", x_test.shape)
+print("y_test original shape", y_test.shape)
+
+x_train = x_train.reshape(x_train.shape[0],28,28,1)
+x_test = x_test.reshape(x_test.shape[0],28,28,1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+print("let's see where we are :",x_train.shape)
+
+number_of_classes = 10;
+
+y_train = kr.utils.np_utils.to_categorical(y_train,number_of_classes)
+y_test = kr.utils.np_utils.to_categorical(y_test,number_of_classes)
+
+x_train = x_train/255
+x_test = x_test/255
+
+model = kr.models.Sequential()
+
+model.add(kr.layers.Conv2D(32,(3,3),input_shape=(28,28,1)))
+kr.layers.normalization.BatchNormalization(axis=-1)
+model.add(kr.layers.Activation('relu'))
+model.add(kr.layers.Conv2D(32,(3,3)))
+kr.layers.normalization.BatchNormalization(axis=-1)
+model.add(kr.layers.Activation('relu'))
+model.add(kr.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(kr.layers.Conv2D(64,(3,3)))
+kr.layers.normalization.BatchNormalization(axis=-1)
+model.add(kr.layers.Activation('relu'))
+model.add(kr.layers.Conv2D(64,(3,3)))
+kr.layers.normalization.BatchNormalization(axis=-1)
+model.add(kr.layers.Activation('relu'))
+model.add(kr.layers.MaxPooling2D(pool_size=(2,2)))
+
+model.add(kr.layers.Flatten())
+model.add(kr.layers.Dense(512))
+kr.layers.normalization.BatchNormalization()
+model.add(kr.layers.Activation('relu'))
+model.add(kr.layers.Dropout(0.2))
+model.add(kr.layers.Dense(10))
+
+model.add(kr.layers.Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',optimizer= kr.optimizers.Adam(), metrics =['accuracy'] )
+
+gen = ImageDataGenerator(rotation_range= 8 , width_shift_range=0.08,shear_range=0.3,height_shift_range=0.08,zoom_range=0.08)
+test_gen = ImageDataGenerator()
+
+train_gen = gen.flow(x_train,y_train,batch_size=64)
+test_generator = gen.flow(x_test,y_test,batch_size=64)
+
+model.fit_generator(train_gen,steps_per_epoch=60000//64, epochs=5, validation_data = test_generator, validation_steps = 10000//64)
+
+acc = (x_test,y_test)
+print()
+print('Accuracy', acc[1])
---
+++ static/js/drawCanvas.js
@@ -0,0 +1,96 @@
+ var canvas, ctx, flag = false,
+        prevX = 0,
+        currX = 0,
+        prevY = 0,
+        currY = 0,
+        dot_flag = false;
+
+    var x = "white",
+        y = 10;
+    
+    function init() {
+        canvas = document.getElementById('myCanvas');
+		outputImg = document.getElementById('canvasimg');
+        ctx = canvas.getContext("2d");
+        w = canvas.width;
+        h = canvas.height;
+    
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
+    }
+    
+ 
+    
+    function draw() {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = x;
+        ctx.lineWidth = y;
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    
+    function save() {
+        outputImg.style.border = "2px solid";
+        var dataURL = canvas.toDataURL();
+		var saveImg = canvas.toDataURL('image/png');
+		outputImg.src = dataURL;
+		outputImg.style.display = "inline";
+		
+		
+    }
+    
+	
+	
+    function findxy(res, e) {
+        if (res == 'down') {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+    
+            flag = true;
+            dot_flag = true;
+            if (dot_flag) {
+                ctx.beginPath();
+                ctx.fillStyle = x;
+                ctx.fillRect(currX, currY, 2, 2);
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+    }
+	
+	function erase() {
+        var m = confirm("Do you want to clear the image?");
+        if (m) {
+            ctx.clearRect(0, 0, w, h);
+            document.getElementById("canvasimg").style.display = "none";
+        }
+    }
+	
+	
